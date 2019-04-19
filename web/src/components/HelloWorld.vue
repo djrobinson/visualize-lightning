32a626dd8d2da8a2ea5684c324395323c4c0d002
Danny Robinson <daniel@djrobinson.me>
2019-04-18 19:22:31 -0600

    Get deck.gl working with Vue

--- web/src/components/HelloWorld.vue
+++ web/src/components/HelloWorld.vue
@@ -1,60 +1,51 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa" target="_blank" rel="noopener">pwa</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest" target="_blank" rel="noopener">unit-jest</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div style="height: 100%">
+          <h1>Test</h1>
+          <canvas id="my-canvas"></canvas>
+    </div>
 </template>
+<script>
+import {Deck, MapController} from '@deck.gl/core';
+import {GeoJsonLayer} from '@deck.gl/layers';
 
-<script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
+export default {
+    data(){
+        console.log("Sanity check")
+        return {
+            deck: null
+        }
+    },
+    mounted(){
+        const GEOJSON =
+          'https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_110m_admin_1_states_provinces_shp.geojson'; //eslint-disable-line
 
-@Component
-export default class HelloWorld extends Vue {
-  @Prop() private msg!: string;
+        const INITIAL_VIEW_STATE = {
+          latitude: 40,
+          longitude: -100,
+          zoom: 3,
+          bearing: 30,
+          pitch: 30
+        };
+
+
+        this.deck = new Deck({
+          canvas: "my-canvas",
+          width: '100%',
+          height: '100%',
+          initialViewState: INITIAL_VIEW_STATE,
+          controller: MapController,
+          layers: [
+            new GeoJsonLayer({
+              data: GEOJSON,
+              stroked: true,
+              filled: true,
+              lineWidthMinPixels: 2,
+              opacity: 0.4,
+              getLineColor: () => [255, 100, 100],
+              getFillColor: () => [200, 160, 0, 180]
+            })
+          ]
+        })
+  }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
