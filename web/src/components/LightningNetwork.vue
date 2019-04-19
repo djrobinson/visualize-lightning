ba9404ad91b7eb452121c2dd78df3ea9bf46e7ff
Danny Robinson <daniel@djrobinson.me>
2019-04-19 11:16:17 -0600

    Mapboxgl & deck.gl working together

--- web/src/components/LightningNetwork.vue
+++ web/src/components/LightningNetwork.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="network-container">
-    <img width="100%" height="100%" src="https://api.mapbox.com/styles/v1/mapbox/light-v9/static/auto/1280x700@2x?access_token=pk.eyJ1IjoiZGFubnkxcm9iaW5zb24iLCJhIjoiY2p1bTExc21tMHliNzN5bXFoNGZua3MzZyJ9.4RybkDpAAixpKuuCmTeEyA" alt="Mapbox Map of -73.7638,42.6564">
     <div class="canvas-container">
-      <canvas id="network-canvas"></canvas>
+      <div id="map"></div>
     </div>
   </div>
 </template>
 <script>
 import {Deck, MapController} from '@deck.gl/core';
-import {GeoJsonLayer, ArcLayer} from '@deck.gl/layers';
+import {MapboxLayer} from '@deck.gl/mapbox';
+import {GeoJsonLayer, ArcLayer, ScatterplotLayer} from '@deck.gl/layers';
 import {scaleQuantile} from 'd3-scale';
+import mapboxgl from 'mapbox-gl';
 import axios from 'axios';
 
 export default {
@@ -20,6 +21,7 @@ export default {
             graph: null,
             pong: null,
             arcs: null,
+            map: null,
             INITIAL_VIEW_STATE: {
               longitude: -100,
               latitude: 40.7,
@@ -38,7 +40,7 @@ export default {
         //     console.log("What are describe nodes looking like?", this.graph.nodes)
         //     console.log("What are describe edges looking like?", this.graph.edges)
         //   })
-
+        mapboxgl.accessToken = 'pk.eyJ1IjoiZGFubnkxcm9iaW5zb24iLCJhIjoiY2p1bTExc21tMHliNzN5bXFoNGZua3MzZyJ9.4RybkDpAAixpKuuCmTeEyA'
         axios
           .get('https://raw.githubusercontent.com/uber-common/deck.gl-data/master/examples/arc/counties.json')
           .then(response => {
@@ -46,10 +48,46 @@ export default {
             console.log("What are describe nodes looking like?", this.data)
             this._recalculateArcs(this.data)
             console.log("Did arcs work? ", this.data)
-            this.deck = this._renderLayers()
+
           })
+
+        const map = new mapboxgl.Map({
+          container: 'map',
+          style: 'mapbox://styles/mapbox/streets-v9',
+          center: [-118.35, 33.83],
+          pitch: 30,
+          zoom: 5
+        });
+        map.on('load', () => {
+          this._mapLoaded(map)
+        });
     },
     methods: {
+      _mapLoaded(map) {
+        
+        const geojsonlayer = new MapboxLayer({
+          id: 'geojson',
+          type: GeoJsonLayer,
+          data: this.data,
+          stroked: false,
+          filled: true,
+          getFillColor: [0, 0, 0, 0],
+          pickable: true
+        })
+        const arclayer = new MapboxLayer({
+          id: 'arc',
+          type: ArcLayer,
+          data: this.arcs,
+          getSourcePosition: d => d.source,
+          getTargetPosition: d => d.target,
+          getWidth: 2
+        })
+
+        map.addLayer(arclayer)
+        map.addLayer(geojsonlayer)
+
+        
+      },
       _recalculateArcs(data, selectedCounty = null) {
         if (!data) {
           return;
@@ -119,7 +157,11 @@ export default {
     padding: 0px;
     border: solid 1px black;
   }
-  #my-canvas {
-    position: relative;
+  #map {
+    width: 100%;
+    height: 500px;
+  }
+  canvas {
+    left: 0;
   }
 </style>
